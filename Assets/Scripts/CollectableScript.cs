77be93cf7bbd86c72b6a3d176fed687f1a3adf7c
jrpsus <90849912+jrpsus@users.noreply.github.com>
2024-02-20 16:10:51 -0500

    all money collectables done

--- Assets/Scripts/CollectableScript.cs
+++ Assets/Scripts/CollectableScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CollectableScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int type;
+    public float yield;
+    float cd = 1;
+    public PlayerScript player;
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerScript>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cd -= Time.deltaTime;
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cd < 0)
+        {
+            if (type == 1)
+            {
+                player.cash += yield;
+            }
+            Destroy(gameObject);
+        }
     }
 }
