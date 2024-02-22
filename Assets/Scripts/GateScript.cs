9fa001fb76ff4e2a98ead69651e3b29e7e930ac2
jrpsus <90849912+jrpsus@users.noreply.github.com>
2024-02-22 16:27:45 -0500

    gates

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    public int gatehp;
+    public int gatemaxhp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
